32c3941033d3638457cf712745ea731be3a0f8c6
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-08-22 20:14:13 +0300

    Replace

---
+++ index.php
@@ -0,0 +1,9 @@
+<? include_once("php/header.php"); ?>
+
+КАЗИНО макСИМА
+<form method = "post">
+    <input type = "submit" name = "bet" value = "Start">
+    <input type = "submit" name = "restart" value = "Restart">
+</form>
+
+<? include_once("php/footer.php"); ?>
